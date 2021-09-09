80c77f672087c0cf228ec3bf8253fe23b8b2392f
lee scroggins <lee_scroggins@yahoo.com>
2021-09-09 11:57:05 -0600

    Added level, scoring system, sound effects on block break.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,23 +7,27 @@ public class Ball : MonoBehaviour
 {
 // config params
     [SerializeField] private Paddle paddle1;
-    [SerializeField] private float xPush = 2f; 
+    [SerializeField] private float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] private AudioClip[] ballSounds;
 
     // state
     private Vector2 paddleToBallVector;
     bool hasStarted = false;
+    
+    // Cached component references
+    private AudioSource myAudioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (!hasStarted)
         {
             LockBallToPaddle();
@@ -43,5 +48,14 @@ public class Ball : MonoBehaviour
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
     }
 }
